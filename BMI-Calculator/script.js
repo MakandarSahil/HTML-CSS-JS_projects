bc95bc39d69c00a1e858189e7d3547188b623d94
MakandarSahil <sahilmakandar15@gmail.com>
2024-10-28 12:17:43 +0530

    BMI JS

--- BMI-Calculator/script.js
+++ BMI-Calculator/script.js
@@ -3,13 +3,16 @@ const form = document.querySelector('form')
 form.addEventListener('submit' ,function(e){
     e.preventDefault();
 
-    const height = parseInt(document.querySelector('#height'));
-    const weight = parseInt(document.querySelector('#weight'));
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
     const results = document.querySelector('#results');
 
     if(height === "" || height <= 0 || isNaN(height)){
-        results.innerHTML = `${height} is not a valid height`
+        results.innerHTML = `please give a valid height`;
     }else if(weight === "" || weight <= 0 || isNaN(weight)){
-        results.innerHTML = `${weight} is not a vali weight`
+        results.innerHTML = `please give a valid weight`;
+    }else{
+        const bmi  =(weight / ((height * height) / 10000)).toFixed(2);
+        results.innerHTML = `BMI = <span>${bmi}</span>`
     }
 })
